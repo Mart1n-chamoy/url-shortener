b3a74cacfc7d030bec39f0da5d3ed100ab7ea466
Mart1n chamoy <2004colussi@gmail.com>
2026-06-01 20:50:06 -0300

    Agregado de stats

--- app/main.py
+++ app/main.py
@@ -1,6 +1,17 @@
 from fastapi import FastAPI
 from app.database import engine, Base
 from app.schemas import URLCreate
+import random
+import string
+from sqlalchemy.orm import Session
+from fastapi import Depends
+
+from app.database import get_db
+from app.models import URL
+
+from fastapi.responses import RedirectResponse
+from fastapi import HTTPException
+
 
 app = FastAPI(
     title="URL Shortener API",
@@ -16,8 +27,62 @@ def home():
 
 
 @app.post("/shorten")
-def shorten_url(url_data: URLCreate):
+def shorten_url(url_data: URLCreate, db: Session = Depends(get_db)):
+
+    short_code = generate_short_code()
+
+    new_url = URL(
+        original_url=str(url_data.url),
+        short_code=short_code
+    )
+
+    db.add(new_url)
+    db.commit()
+    db.refresh(new_url)
+
     return {
-        "url_recibida": url_data.url
+        "original_url": new_url.original_url,
+        "short_code": new_url.short_code
     }
-    
+
+def generate_short_code(length=6):
+    characters = string.ascii_letters + string.digits
+    return ''.join(random.choice(characters) for _ in range(length))
+
+
+@app.get("/{short_code}")
+def redirect_url(short_code: str, db: Session = Depends(get_db)):
+
+    url = db.query(URL).filter(
+        URL.short_code == short_code
+    ).first()
+
+    if not url:
+        raise HTTPException(
+            status_code=404,
+            detail="URL no encontrada"
+)
+
+    url.clicks += 1
+    db.commit()
+
+    return RedirectResponse(url=url.original_url)    
+
+@app.get("/stats/{short_code}")
+def get_stats(short_code: str, db: Session = Depends(get_db)):
+
+    url = db.query(URL).filter(
+        URL.short_code == short_code
+    ).first()
+
+    if not url:
+        raise HTTPException(
+            status_code=404,
+            detail="URL no encontrada"
+        )
+
+    return {
+        "original_url": url.original_url,
+        "short_code": url.short_code,
+        "clicks": url.clicks
+    }
--- app/schemas.py
+++ app/schemas.py
@@ -1,5 +1,5 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, HttpUrl
 
 
 class URLCreate(BaseModel):
-    url: str
+    url: HttpUrl

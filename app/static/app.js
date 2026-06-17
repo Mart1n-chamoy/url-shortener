a7b4c017228dda07551e36c4b64a7b0cbe701364
Mart1n chamoy <2004colussi@gmail.com>
2026-06-17 16:58:40 -0300

    nueva interfaz

--- app/static/app.js
+++ app/static/app.js
@@ -17,12 +17,87 @@ async function shortenUrl() {
 
     const data = await response.json();
 
+    if (!response.ok) {
+
+        document.getElementById("result").innerHTML = `
+            <div class="result-box">
+                ❌ ${data.detail}
+            </div>
+        `;
+
+        return;
+    }
+
+    const shortUrl =
+        `${window.location.origin}/${data.short_code}`;
+
     document.getElementById("result").innerHTML = `
-        <p>
-            URL corta:
-            <a href="/${data.short_code}">
-                ${window.location.origin}/${data.short_code}
+        <div class="result-box">
+
+            <strong>URL corta:</strong>
+
+            <br><br>
+
+            <a href="${shortUrl}" target="_blank">
+                ${shortUrl}
             </a>
-        </p>
+
+            <br><br>
+
+            <button onclick="copyUrl('${shortUrl}')">
+                Copiar URL
+            </button>
+
+        </div>
     `;
+}
+
+async function getStats() {
+
+    const code =
+        document.getElementById("stats_code").value;
+
+    const response =
+        await fetch(`/stats/${code}`);
+
+    const data = await response.json();
+
+    if (!response.ok) {
+
+        document.getElementById("stats").innerHTML = `
+            <div class="result-box">
+                ❌ Código no encontrado
+            </div>
+        `;
+
+        return;
+    }
+
+    document.getElementById("stats").innerHTML = `
+        <div class="result-box">
+
+            <p>
+                <strong>URL:</strong><br>
+                ${data.original_url}
+            </p>
+
+            <p>
+                <strong>Código:</strong>
+                ${data.short_code}
+            </p>
+
+            <p>
+                <strong>Clicks:</strong>
+                ${data.clicks}
+            </p>
+
+        </div>
+    `;
+}
+
+function copyUrl(url) {
+
+    navigator.clipboard.writeText(url);
+
+    alert("URL copiada");
 }
